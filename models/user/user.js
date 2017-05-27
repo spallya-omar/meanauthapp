8ccebf7bae6ccb6187922d04ab95d1daf6cb9888
spallya-omar <spallya@outlook.com>
2017-05-27 15:59:07 +0530

    Added Promise and changed folder structure and also added index

--- models/user/user.js
+++ models/user/user.js
@@ -1,27 +1,8 @@
 const mongoose = require(`mongoose`);
 const bcrypt = require(`bcryptjs`);
-const dbconfig = require(`../config/database`);
-
-// User Schema
-const UserSchema = mongoose.Schema({
-    name: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-});
-
-const User = module.exports = mongoose.model('User', UserSchema);
+const Q = require('q');
+const dbconfig = require(`../../config/database`);
+const User = require('./userSchema');
 
 module.exports = {
 
@@ -34,14 +15,22 @@ module.exports = {
         User.findOne(query, callback);
     },
 
-    addUser: function(newUser, callback) {
+    addUser: function(newUser) {
+        let deferred = new Q.defer();
         bcrypt.genSalt(10, (err, salt) => {
             if(err) throw err;
             bcrypt.hash(newUser.password, salt, (err, hash) => {
                 if(err) throw err;
                 newUser.password = hash;
-                newUser.save(callback);
+                newUser.save((err)=>{
+                  if(err){
+                    deferred.resolve({success:false, message:`Error saving the user ${newUser.username}`});
+                  }else{
+                    deferred.resolve({success:true, message:`User ${newUser.username} saved successfully`});
+                  }
+                });
             });
         });
+        return deferred.promise;
     }
 }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,23 +2,23 @@ const router = require(`express`).Router();
 const passport = require(`passport`);
 const jwt = require(`jsonwebtoken`);
 const dbconfig = require(`../config/database`);
-const User = require(`../models/user`);
+const UserIndex = require(`../models/user`);
 
 // Register
 router.post(`/register`, (req, res, next) => {
-    let newUser = new User({
-        name: req.body.name,
+    let newUser = new UserIndex.UserSchema({
+        name:  req.body.name,
         email: req.body.email,
         username: req.body.username,
         password: req.body.password
     });
-    User.addUser(newUser, (err, user) => {
-        if(err) {
-            res.json({success: false, msg: `Failed to register User`});
-        } else {
-            res.json({success: true, msg: `User registered successfully`});
-        }
+    UserIndex.helpers.addUser(newUser).then((response)=>{
+      res.json(response);
     });
+});
+
+router.post('/test',(req,res)=>{
+
 });
 
 // Authenticate
